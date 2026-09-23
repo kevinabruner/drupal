2f2af620afb6d1aa140a646b70dc9824089d038b
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-09-22 21:46:35 -0400

    big oof

--- roles/first_boot/templates/kevin-bootstrap.sh
+++ roles/first_boot/templates/kevin-bootstrap.sh
@@ -17,6 +17,7 @@ a2enconf Drupal-env
 systemctl restart apache2
 
 # If the hostname ends in 1 and we're in prod, then create a cron job to backup nfs
-if [[ "$(hostname)" == *1 && "$environment" == "prod" ]]; then
-    cat "0 3 * * * root rsync -az --delete /var/www/html/sites/default/files/ /home/kevin/nfs-files" > /etc/cron.d/ceph-nfs-backup
+if [[ "$(hostname -s)" == *1 && "$environment" == "prod" ]]; then
+    echo "0 3 * * * root rsync -az --delete /var/www/html/sites/default/files/ /home/kevin/nfs-files" > /etc/cron.d/ceph-nfs-backup
+    chmod 644 /etc/cron.d/ceph-nfs-backup
 fi
